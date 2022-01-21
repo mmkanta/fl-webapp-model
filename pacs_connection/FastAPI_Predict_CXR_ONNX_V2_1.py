0cad17434adec1772b547b745f868dceb0510043
phare <fairphare@gmail.com>
2022-01-22 00:20:23 +0700

    fix: random color, write ratio on top-left

--- pacs_connection/FastAPI_Predict_CXR_ONNX_V2_1.py
+++ pacs_connection/FastAPI_Predict_CXR_ONNX_V2_1.py
@@ -248,15 +248,27 @@ def plot_bbox_from_df(df_bbox, image_path):
     import pydicom
     import numpy as np
     import pandas as pd
+    import random
 
     all_bbox = pd.DataFrame(df_bbox['data'], columns=['label', 'tool', 'data'])
+
+    #generate random class-color mapping
+    all_class = all_bbox["label"].unique()
+    cmap = dict()
+    for lb in all_class:
+        while True:
+            b = random.randint(0,255)
+            g = random.randint(0,255)
+            r = random.randint(0,255)
+            if (b,g,r) not in cmap.values():
+                cmap[lb] = (b,g,r)
+                break
+    all_bbox["color"] = all_bbox["label"].map(cmap)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     fontScale = 1.5
     thickness = 6
     isClosed = True
-    # Green color
-    color = (0, 255, 0)
 
     ### ส่วนที่เรียก image
     #import dicom image array
@@ -280,14 +292,15 @@ def plot_bbox_from_df(df_bbox, image_path):
         inputImage = inputImage.astype(int)
         inputImage = cv2.UMat(inputImage).get()
 
+    all_ratio = []
     for index, row in all_bbox.iterrows():
 
         if row['tool'] == 'rectangleRoi':
             pts = row["data"]["handles"]
             inputImage = cv2.rectangle(inputImage, (int(pts["start"]["x"]), int(pts["start"]["y"])), (int(
-                pts["end"]["x"]), int(pts["end"]["y"])), color, thickness)
+                pts["end"]["x"]), int(pts["end"]["y"])), row["color"], thickness)
             inputImage = cv2.putText(inputImage,  row["label"], (int(min(pts["start"]["x"], pts["end"]["x"])), int(
-                min(pts["start"]["y"], pts["end"]["y"]))), font, fontScale, color, thickness, cv2.LINE_AA)
+                min(pts["start"]["y"], pts["end"]["y"]))), font, fontScale, row["color"], thickness, cv2.LINE_AA)
 
         if row['tool'] == 'freehand':
             pts = np.array([[cdn["x"], cdn["y"]]
@@ -295,9 +308,9 @@ def plot_bbox_from_df(df_bbox, image_path):
             pts = pts.reshape((-1, 1, 2))
             #choose min x,y for text origin
             text_org = np.amin(pts, axis=0)
-            inputImage = cv2.polylines(inputImage, [pts], isClosed, color, thickness)
+            inputImage = cv2.polylines(inputImage, [pts], isClosed, row["color"], thickness)
             inputImage = cv2.putText(inputImage,  row["label"], tuple(
-                text_org[0]), font, fontScale, color, thickness, cv2.LINE_AA)
+                text_org[0]), font, fontScale, row["color"], thickness, cv2.LINE_AA)
 
         if row['tool'] == 'length':
             pts = row["data"]["handles"]
@@ -306,29 +319,16 @@ def plot_bbox_from_df(df_bbox, image_path):
             if pts["start"]["x"] > pts["end"]["x"]:
                 text_org = "end"
             inputImage = cv2.line(inputImage, (int(pts["start"]["x"]), int(pts["start"]["y"])), (int(
-                pts["end"]["x"]), int(pts["end"]["y"])), color, thickness)
+                pts["end"]["x"]), int(pts["end"]["y"])), row["color"], thickness)
             inputImage = cv2.putText(inputImage,  row["label"], (int(pts[text_org]["x"]), int(
-                pts[text_org]["y"])), font, fontScale, color, thickness, cv2.LINE_AA)
+                pts[text_org]["y"])), font, fontScale, row["color"], thickness, cv2.LINE_AA)
 
         if row['tool'] == 'ratio':
-            pts = row["data"]["0"]["handles"]
-            #choose left point for text origin
-            text_org = "start"
-            if pts["start"]["x"] > pts["end"]["x"]:
-                text_org = "end"
-            inputImage = cv2.line(inputImage, (int(pts["start"]["x"]), int(pts["start"]["y"])), (int(
-                pts["end"]["x"]), int(pts["end"]["y"])), color, thickness)
-            inputImage = cv2.putText(inputImage,  row["label"], (int(pts[text_org]["x"]), int(
-                pts[text_org]["y"])), font, fontScale, color, thickness, cv2.LINE_AA)
-            pts = row["data"]["1"]["handles"]
-            #choose left point for text origin
-            text_org = "start"
-            if pts["start"]["x"] > pts["end"]["x"]:
-                text_org = "end"
-            inputImage = cv2.line(inputImage, (int(pts["start"]["x"]), int(pts["start"]["y"])), (int(
-                pts["end"]["x"]), int(pts["end"]["y"])), color, thickness)
-            inputImage = cv2.putText(inputImage,  row["label"], (int(pts[text_org]["x"]), int(
-                pts[text_org]["y"])), font, fontScale, color, thickness, cv2.LINE_AA)
+            all_ratio.append((row["label"],row["data"]["ratio"],row["color"]))
+    
+    #write all ratios on top-left
+    for i in range(len(all_ratio)):
+        inputImage = cv2.putText(inputImage,  "{} ({})".format(all_ratio[i][0],all_ratio[i][1]), (10,50+60*i), font, fontScale, all_ratio[i][2], thickness, cv2.LINE_AA)
 
         #cv2.imshow(inputImage)
         #cv2.imwrite("/path/to/save.png", inputImage)
